a38de0eebf823b2e437d1af05d0f16a4f2635f31
Dawod Hossen <dawod.analyst@gmail.com>
2025-10-22 18:26:54 +0000

    POSITION : SENIOR SOFTWARE. ENGINEER [ FULL STACK ]

--- src/components/icons/TalentoLogo.tsx
+++ src/components/icons/TalentoLogo.tsx
@@ -2,15 +2,42 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 export const TalentoLogo = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 577.4 153.2"
-    {...props}
-  >
-    <path
-      fill="currentColor"
-      d="M111.4 151.2v-112h-32v-32h96v32h-32v112zM212.4 151.2V7.2h32v144zM320.4 151.2l-32-48-32 48h-37l50-74-49-70h37l31 46 31-46h37l-49 70 50 74zM433.4 151.2v-144h96v32h-64v32h56v32h-56v48zM577.4 87.2c0 38-25 64-64 64s-64-26-64-64 25-64 64-64 64 26 64 64zm-96 0c0 19 12 32 32 32s32-13 32-32-12-32-32-32-32 13-32 32z"
-    />
-    <path fill="#FF7043" d="M36.4 119.2h32v32h-32z" />
-  </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 400 100"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M50 15 C 25 15, 25 50, 50 50 C 75 50, 75 15, 50 15 M 50 20 C 69.5 20, 69.5 45, 50 45 C 30.5 45, 30.5 20, 50 20 M 50 25 C 64 25, 64 40, 50 40 C 36 40, 36 25, 50 25 M 50 30 C 58.5 30, 58.5 35, 50 35 C 41.5 35, 41.5 30, 50 30 M 50 34 A 1 1 0 0 0 50 36 A 1 1 0 0 0 50 34"
+      />
+      <path
+        fill="currentColor"
+        stroke="currentColor"
+        strokeWidth="3"
+        d="M 50, 15 A 35,35 0 1,1 49.9,15"
+        strokeLinecap="round"
+      />
+      <text
+        x="100"
+        y="50"
+        fontFamily="sans-serif"
+        fontSize="30"
+        fill="currentColor"
+        fontWeight="bold"
+        dominantBaseline="middle"
+      >
+        TALENTO
+      </text>
+      <text
+        x="100"
+        y="68"
+        fontFamily="sans-serif"
+        fontSize="8"
+        fill="currentColor"
+        dominantBaseline="middle"
+      >
+        SEAMLESS TALENT SOLUTIONS.
+      </text>
+    </svg>
 );
